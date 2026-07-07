b7677231c37c1fb794558c2abb435617fa403301
ikequasie2003-source <ikequasie2003@github.com>
2026-07-07 13:38:57 +0000

    Media resolver fetches from Supabase, images show on live site

--- push.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-"C:\Program Files\Git\cmd\git.exe" -C "C:\Users\QUASIE\Documents\isqstudios-main" add .
-"C:\Program Files\Git\cmd\git.exe" -C "C:\Users\QUASIE\Documents\isqstudios-main" commit -m "Media resolver fetches from Supabase, images show on live site"
-"C:\Program Files\Git\cmd\git.exe" -C "C:\Users\QUASIE\Documents\isqstudios-main" push origin master:main > "C:\Users\QUASIE\Documents\isqstudios-main\push_out.txt" 2>&1
-echo Exit: %ERRORLEVEL% >> "C:\Users\QUASIE\Documents\isqstudios-main\push_out.txt"
